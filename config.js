283eff57a30da4cadb4a24957d5b2ff308b3eedd
Jonas Millan <Jonas_Millan@intranet.itresources>
2019-06-21 13:42:14 -0300

    pepe

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
   port: process.env.PORT || 3001,
-  MONGODB_URI: process.env.PORT ||'mongodb+srv://admin:admin1@cluster0-2rj6q.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  MONGODB_URI: 'mongodb+srv://admin:admin1@cluster0-2rj6q.mongodb.net/Cluster0?retryWrites=true&w=majority'
 };
 module.exports = config;
