90d71040ff9704c12f293360f580639e3e8516db
Jonas Millan <Jonas_Millan@intranet.itresources>
2019-06-21 13:25:34 -0300

    plz heroku

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true }, async (error, re
     return console.log(`Connection error ${error}`);
   }
   console.log("DB Connection on ");
-  app.listen(config.port, () => {
-    console.log(`Hamster rolling on port ${config.port}`);
+  app.listen(process.env.PORT || 3001, () => {
+    console.log(`Hamster rolling`);
   });
   await seeds()
 });
